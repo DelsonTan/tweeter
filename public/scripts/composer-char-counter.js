11bd4e8d61da0df648adbb64ecf0a4cdb0e1cdd0
Delson Tan <delsonwtan@gmail.com>
2018-07-17 21:10:59 +0000

    added layout for tweet, adjusted overall layout for appearance

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     const textAreaSelector = $("main .new-tweet textarea");
     const counterSelector = $("main .new-tweet form .counter");
+    
     textAreaSelector.on('input', function() {
-         const counterVal = 140 - textAreaSelector.val().length;
+        const counterVal = 140 - textAreaSelector.val().length;
         if (counterVal < 0) {
             counterSelector.text(counterVal).css('color','red');
         } else {
