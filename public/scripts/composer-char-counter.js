a004e2a07078a033b2528f2bfaf6b5a8615c87e5
Delson Tan <delsonwtan@gmail.com>
2018-07-17 03:40:19 +0000

    Character counter for tweet text area now updates dynamically when content in the textbox changes, turns red when char limit is exceeded and black when it is not

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-    console.log("working!");
+    const textAreaSelector = $("main .new-tweet textarea");
+    const counterSelector = $("main .new-tweet form .counter");
+    textAreaSelector.on('input', function() {
+         const counterVal = 140 - textAreaSelector.val().length;
+        if (counterVal < 0) {
+            counterSelector.text(counterVal).css('color','red');
+        } else {
+            counterSelector.text(counterVal).css('color','black');
+        }
+      });
   });
