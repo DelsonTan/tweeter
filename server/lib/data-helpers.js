d7601ec0050b1b4e1eb1050b12c141e40d69c860
Delson Tan <delsonwtan@gmail.com>
2018-07-20 00:31:00 +0000

    Update the README to reflect dependencies up to this point, and personalize it a little

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -11,10 +11,8 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
         db.collection("tweets").find().sort(sortNewestFirst).toArray(callback); 
     }
-
   };
 }
